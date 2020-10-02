d2cd33f517c7b3a83cddf22509c046b2667717d9
Vladimir Melnichenko <v.melnichenko@crazypanda.ru>
2020-10-02 16:15:50 +0300

    pod + test fixes

--- lib/JSON/Immutable/XS.pm
+++ lib/JSON/Immutable/XS.pm
@@ -11,39 +11,40 @@ JSON::Immutable::XS
 
 =cut
 
-our $VERSION = '0.1.2';
+our $VERSION = '0.1.3';
 XS::Loader::bootstrap();
 
 =head1 DESCRIPTION
 
 Fast and simple abstract node-tree based storage with JSON support. Usefull for reading JSON configs, keeping it in the memory and don't worrying about mutability. Has XPath-like interface for data accessing. Easy to use with XS and C/C++ modules.
-It use RapidJSON as JSON parser. https://rapidjson.org/
+It use RapidJSON as JSON parser. L<https://rapidjson.org/>
 
 =cut
 
 =head1 SYNOPSIS
-C<use JSON::Immutable::XS;>
 
-# you can provide this variable to XS and use it there
-my $dict = JSON::Immutable::XS->new('example/dict.json');
+    use JSON::Immutable::XS;
 
-# get subnode by path ( any node is JSON::Immutable::XS ref)
-my $node = $dict->get( 'path', 2, 'node'); # similar to ->{path}[2]{node}
-# get() returns undef on not existed path
+    # you can provide this variable to XS and use it there
+    my $dict = JSON::Immutable::XS->new('example/dict.json');
 
-my $data = $node->get_value('this', 'is', 'the', 'way'); # data could be an any Perl Sv structure ( scalar, hash or array )
+    # get subnode by path ( any node is JSON::Immutable::XS ref)
+    my $node = $dict->get( 'path', 2, 'node'); # similar to ->{path}[2]{node}
 
-# unless path will found - it'll return undef
+    # get() returns undef on not existed path
+    my $data = $node->get_value('this', 'is', 'the', 'way'); # data could be an any Perl Sv structure ( scalar, hash or array )
 
-# it possible to get keys from node as well
-for ( @{$dict->keys()} ){
-...
-}
+    # unless path will found - it'll return undef
+    # it possible to get keys from node as well
 
-# use slice to get exactly that you need without unnecessary data ( detail description below )>
-$dict->slice( 'path' );
+    for ( @{$dict->keys()} ){
+    ...
+    }
 
-# see more features below
+    # use slice to get exactly that you need without unnecessary data ( detail description below )>
+    $dict->slice( 'path' );
+
+    # see more features below
 
 =cut
 
@@ -75,13 +76,14 @@ Same as get_value() without path
 =head1 AUTHOR
 
 Vladimir Melnichenko <melnichenkovv@gmail.com>, Crazy Panda, CP Decision LTD
-<https://github.com/VMELNICHENKO/JSON-Immutable-XS>
+
+L<https://github.com/VMELNICHENKO/JSON-Immutable-XS>
 
 =cut
 
 =head1 LICENSE
 
-You may distribute this code under the same terms as RapidJSON itself. https://rapidjson.org/
+You may distribute this code under the same terms as RapidJSON itself. L<https://rapidjson.org/>
 
 =cut
 
